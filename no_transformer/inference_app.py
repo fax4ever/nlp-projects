13e9fc322943951fd1f09e42f0d16be2b84204c8
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2025-05-04 19:44:37 +0200

    multi modal device

--- no_transformer/inference_app.py
+++ no_transformer/inference_app.py
@@ -15,29 +15,30 @@ def number_to_label(label):
     raise ValueError('label not suppoerted: ' + label)
 
 def main():
-    set_seed(42)
-    model = MultiModalModel.from_pretrained("fax4ever/no-transformer-test")
-    processed_dataset = ProcessedDataset()
-
+    model = MultiModalModel.from_pretrained("fax4ever/culturalitems-no-transformer")
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print("using device:", device)
+    model.to_device(device)
+    model.eval()
     matching = 0
     with torch.no_grad():
         validation = processed_dataset.validation()
         for entity in DataLoader(validation):
-            prediction = model(entity).detach().clone().argmax(dim=1).numpy()[0]
+            prediction = model(entity).detach().cpu().clone().argmax(dim=1).numpy()[0]
             true_label = entity['output_label'].numpy()[0]
             match = prediction == true_label
             if match:
                 matching = matching + 1
     print('matched', matching, 'on', len(validation), '(', matching/len(validation), ')')
 
-    with open('Lost_in_Language_Recognition_output_multimodalnn.csv', 'w', newline='') as file:
+    with open(BASE_PATH + 'Lost_in_Language_Recognition_output_multimodalnn.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         field = ['item', 'name', 'label']
         writer.writerow(field)
         with torch.no_grad():
             test = processed_dataset.test()
             for entity in DataLoader(test):
-                prediction = model(entity).detach().clone().argmax(dim=1).numpy()[0]
+                prediction = model(entity).detach().cpu().clone().argmax(dim=1).numpy()[0]
                 writer.writerow(["http://www.wikidata.org/entity/" + entity['entity_id'][0], entity['name'][0], number_to_label(prediction)])
 
 if __name__ == "__main__":
--- no_transformer/multi_modal_model.py
+++ no_transformer/multi_modal_model.py
@@ -11,29 +11,42 @@ class MultiModalModel(nn.Module, PyTorchModelHubMixin,
                       repo_url="fax4ever/culturalitems-no-transformer",
                       pipeline_tag="text-classification",
                       license="apache-2.0"):
-    def __init__(self, params, device) -> None:
+    def __init__(self, params):
         super(MultiModalModel, self).__init__()
-        self.device = device
         # individual input layers for frequency vectors
-        self.desc = rescale_vector_layer(params['desc']).to(device)
-        self.wiki = rescale_vector_layer(params['wiki']).to(device)
-        self.labels = rescale_vector_layer(params['labels']).to(device)
-        self.descriptions = rescale_vector_layer(params['descriptions']).to(device)
-        self.aliases = rescale_vector_layer(params['aliases']).to(device)
-        self.pages = rescale_vector_layer(params['pages']).to(device)
-        self.claims = rescale_vector_layer(params['claims']).to(device)
+        self.desc = rescale_vector_layer(params['desc'])
+        self.wiki = rescale_vector_layer(params['wiki'])
+        self.labels = rescale_vector_layer(params['labels'])
+        self.descriptions = rescale_vector_layer(params['descriptions'])
+        self.aliases = rescale_vector_layer(params['aliases'])
+        self.pages = rescale_vector_layer(params['pages'])
+        self.claims = rescale_vector_layer(params['claims'])
         # individual input layers for scalar value
-        self.category = nn.Linear(params['category_dim'], params['category_scale']).to(device)
-        self.type_proj = nn.Linear(params['type_dim'], params['type_scale']).to(device)
+        self.category = nn.Linear(params['category_dim'], params['category_scale'])
+        self.type_proj = nn.Linear(params['type_dim'], params['type_scale'])
         # glove
-        self.desc_glove = rescale_vector_layer(params['desc_glove']).to(device)
+        self.desc_glove = rescale_vector_layer(params['desc_glove'])
         # common classifier
         self.classifier = nn.Sequential(
             nn.Linear(params['total_scale'], params['hidden_layers']),
             nn.ReLU(),
             nn.Dropout(params['dropout']),
             nn.Linear(params['hidden_layers'], 3)
-        ).to(device)
+        )
+
+    def to_device(self, device):
+        self.device = device
+        self.desc = self.desc.to(device)
+        self.wiki = self.wiki.to(device)
+        self.labels = self.labels.to(device)
+        self.descriptions = self.descriptions.to(device)
+        self.aliases = self.aliases.to(device)
+        self.pages = self.pages.to(device)
+        self.claims = self.claims.to(device)
+        self.category = self.category.to(device)
+        self.type_proj = self.type_proj.to(device)
+        self.desc_glove = self.desc_glove.to(device)
+        self.classifier = self.classifier.to(device)
 
     def forward(self, dataset_items):
         desc_feat = self.desc(dataset_items['desc'].to(self.device))
