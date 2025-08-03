fa4f3290c6c086d5ba761fc82858948daf08f2e9
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2025-08-03 12:19:02 +0200

    Working with aggregations

--- sentence_splitter/sentence_splitter_embedding_model/src/sentence_splitter_embedding_model/splitter_with_encoder.py
+++ sentence_splitter/sentence_splitter_embedding_model/src/sentence_splitter_embedding_model/splitter_with_encoder.py
@@ -2,33 +2,12 @@ from transformers import AutoTokenizer
 from datasets import DatasetDict
 
 
-def align_labels_with_tokens(labels, word_ids):
-    new_labels = []
-    current_word = None
-    for word_id in word_ids:
-        if word_id != current_word:
-            # Start of a new word!
-            current_word = word_id
-            label = -100 if word_id is None else labels[word_id]
-            new_labels.append(label)
-        elif word_id is None:
-            # Special token
-            new_labels.append(-100)
-        else:
-            # Same word as previous token
-            label = labels[word_id]
-            # If the label is B-XXX we change it to I-XXX
-            if label % 2 == 1:
-                label += 1
-            new_labels.append(label)
-
-    return new_labels
-
-
 class SplitterWithEncoder:
 
     def __init__(self):
-        pass
+        self.labels = []
+        self.word_ids = []
+        self.new_labels = []
 
     def train(self, dataset_dict:DatasetDict):
         base_embedding_model_name = "bert-base-cased"
@@ -38,7 +17,7 @@ class SplitterWithEncoder:
             self.tokenize_and_align_labels,
             batched=True,
             remove_columns=dataset_dict["train"].column_names,
-            batch_size=256,
+            batch_size=128,
         )
         label_names = [0, 1]
 
@@ -46,8 +25,30 @@ class SplitterWithEncoder:
         tokenized_inputs = self.tokenizer(
             items["token"], is_split_into_words=True
         )
-        tokenized_inputs["labels"] = align_labels_with_tokens(items["label"], tokenized_inputs.word_ids(0))
-        return tokenized_inputs
+        tokenized_inputs["labels"] = self.align_labels_with_tokens(items["label"], tokenized_inputs.word_ids(0))
+        return [tokenized_inputs] # we aggregate each batch of words
 
-    def load_model(self):
-        pass
+    def align_labels_with_tokens(self, labels, word_ids):
+        new_labels = []
+        current_word = None
+        for word_id in word_ids:
+            if word_id != current_word:
+                # Start of a new word!
+                current_word = word_id
+                label = -100 if word_id is None else labels[word_id]
+                new_labels.append(label)
+            elif word_id is None:
+                # Special token
+                new_labels.append(-100)
+            else:
+                # Same word as previous token
+                label = labels[word_id]
+                # If the label is B-XXX we change it to I-XXX
+                if label % 2 == 1:
+                    label += 1
+                new_labels.append(label)
+
+        self.labels.append(labels)
+        self.new_labels.append(new_labels)
+        self.word_ids.append(word_ids)
+        return new_labels
