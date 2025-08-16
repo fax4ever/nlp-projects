a13a5986f381c7f58cc6f2adecacb50348c59468
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2025-08-16 16:33:03 +0200

    Fix colabs and continue report

--- sentence_splitter/sentence_splitter_embedding_model/tests/test_out_of_domain.py
+++ sentence_splitter/sentence_splitter_embedding_model/tests/test_out_of_domain.py
@@ -6,3 +6,22 @@ def test_eval_model():
     value = eval_model("bert-base-cased-sentence-splitter", Path(__file__).parent / "Cuore-GOLD.txt", 6)
     print(value)
     print(value["f1"])
+
+
+def test_text():
+    text = """Non era un legno di lusso, ma un semplice pezzo
+da catasta, di quelli che d’inverno si mettono nelle
+stufe e nei caminetti per accendere il fuoco e per riscaldare le stanze.
+Non so come andasse, ma il fatto gli è che un bel
+giorno questo pezzo di legno capitò nella bottega
+di un vecchio falegname, il quale aveva nome mastr’Antonio, se non che tutti lo chiamavano maestro
+Ciliegia, per via della punta del suo naso, che era
+sempre lustra e paonazza, come una ciliegia matura.
+Appena maestro Ciliegia ebbe visto quel pezzo di
+legno, si rallegrò tutto; e dandosi una fregatina di
+mani per la contentezza, borbottò a mezza voce:
+"Questo legno è capitato a tempo; voglio servirmene per fare una gamba di tavolino." 
+"""
+    text = text.splitlines()
+    text = " ".join(text)
+    print(text)
